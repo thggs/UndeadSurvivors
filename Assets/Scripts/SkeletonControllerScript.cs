98b985547cd051bec9697ec7d1e027ab61882ce8
thggs <ddrodfer@gmail.com>
2023-05-25 15:41:52 +0100

    New assets

--- Assets/Scripts/SkeletonControllerScript.cs
+++ Assets/Scripts/SkeletonControllerScript.cs
@@ -1,35 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SkeletonControllerScript : MonoBehaviour
 {
    Transform playerTransform;  
-   public float speed;
+   public float followSpeed;
+   public float closeInSpeed;
    public float longDistance;
    public float shortDistance;
+
+   NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
         playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        Vector3 toPlayer = playerTransform.position - transform.position;
         if(Vector3.Distance(transform.position, playerTransform.position) > longDistance)
         {
-            transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, speed*Time.deltaTime);
+            agent.destination = playerTransform.position;
+            Debug.Log("Closing Distance...");
             
         }
         else if(Vector3.Distance(transform.position, playerTransform.position) < shortDistance)
         {
-            transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, -speed*Time.deltaTime);;
+            Vector3 destination = toPlayer.normalized * -shortDistance;
+            agent.destination = destination;
+            Debug.Log("Stepping away to " + destination);
         }
-        Vector3 direction = transform.position - playerTransform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        /*float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);*/
        
     }
 }
