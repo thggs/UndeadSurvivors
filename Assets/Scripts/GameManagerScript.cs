a141f015ec8f24cd43f5d65fdc84a44f591fcf57
thggs <ddrodfer@gmail.com>
2023-06-02 11:02:19 +0100

    Update GameManagerScript.cs

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -5,33 +5,42 @@ using UnityEngine.AI;
 
 public class GameManagerScript : MonoBehaviour
 {
+
     public GameObject[] enemyList;
     public new Camera camera;
+    public bool spawnEnemies;
+    public float timeBetweenSpawns;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-        
+        StartCoroutine(Spawn());
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        float randomX = Random.Range(1f, 1.5f);
-        float randomY = Random.Range(1f, 1.5f);
-        if(Random.Range(0f,1f) > 0.5)
-        {
-            randomX = -(randomX - 1);
-        }
-        if(Random.Range(0f,1f) > 0.5)
-        {
-            randomY = -(randomY - 1);
-        }
-        Vector3 spawnPosition = camera.ViewportToWorldPoint(new Vector3(randomX, randomY, camera.nearClipPlane));
-        NavMeshHit hit;
-        NavMesh.SamplePosition(spawnPosition, out hit,Mathf.Infinity, NavMesh.AllAreas);
-        Instantiate(enemyList[0],hit.position,Quaternion.identity);
 
+    }
+
+    private IEnumerator Spawn() {
+        if(spawnEnemies){
+            float randomX = Random.Range(1f, 1.5f);
+            float randomY = Random.Range(1f, 1.5f);
+            if(Random.Range(0f,1f) > 0.5)
+            {
+                randomX = -(randomX - 1);
+            }
+            if(Random.Range(0f,1f) > 0.5)
+            {
+                randomY = -(randomY - 1);
+            }
+            Vector3 spawnPosition = camera.ViewportToWorldPoint(new Vector3(randomX, randomY, camera.nearClipPlane));
+            NavMeshHit hit;
+            NavMesh.SamplePosition(spawnPosition, out hit,Mathf.Infinity, NavMesh.AllAreas);
+            Instantiate(enemyList[0],hit.position,Quaternion.identity);
+        }
+        yield return new WaitForSeconds(timeBetweenSpawns);
     }
 }
