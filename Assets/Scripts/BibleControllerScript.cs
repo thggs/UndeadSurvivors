344e851b7a011ec6bfcd070e6aae5160784b722e
thggs <ddrodfer@gmail.com>
2023-07-03 18:16:25 +0100

    Update Bible

--- Assets/Scripts/BibleControllerScript.cs
+++ Assets/Scripts/BibleControllerScript.cs
@@ -15,33 +15,39 @@ public class BibleControllerScript : MonoBehaviour
         StartCoroutine(Bible());
     }
 
+    public void LevelUp()
+    {
+        
+    }
+
     IEnumerator Bible()
     {
         GameObject[] bibleSpawns = new GameObject[gameStats.bible.BibleLevel];
 
         Quaternion rotation = Quaternion.identity;
 
-        for (int i = 0; i < gameStats.bible.BibleLevel; i++)
+        for (int i = 1; i <= gameStats.bible.BibleLevel; i++)
         {
             // Create object that holds the bible projectiles and set it to follow the player
-            bibleSpawns[i] = new GameObject("BibleSpawn" + (i+1));
-            bibleSpawns[i].transform.position = transform.position;
-            bibleSpawns[i].transform.rotation = rotation;
-            bibleSpawns[i].AddComponent<CameraControllerScript>();
-            bibleSpawns[i].GetComponent<CameraControllerScript>().player = transform.parent;
+            bibleSpawns[i-1] = new GameObject("BibleSpawn" + i);
+            bibleSpawns[i-1].transform.position = transform.position;
+            bibleSpawns[i-1].transform.rotation = rotation;
+            bibleSpawns[i-1].AddComponent<CameraControllerScript>();
+            bibleSpawns[i-1].GetComponent<CameraControllerScript>().player = transform.parent;
 
             // Instantiate Bible as child object of BibleSpawn
-            GameObject instance = Instantiate(bible, Vector3.zero, Quaternion.identity, bibleSpawns[i].transform);
+            GameObject instance = Instantiate(bible, Vector3.zero, Quaternion.identity, bibleSpawns[i-1].transform);
             instance.GetComponent<SingleDamageScript>().damage = gameStats.bible.BibleDamage;
+
             // Rotate Bible around
             rotation.eulerAngles += new Vector3(0, 0, (360 / gameStats.bible.BibleLevel));
         }
 
         yield return new WaitForSeconds(gameStats.bible.BibleLifetime);
 
-        for (int i = 0; i < gameStats.bible.BibleLevel; i++)
+        for (int i = 1; i <= gameStats.bible.BibleLevel; i++)
         {
-            Animator anim = bibleSpawns[i].GetComponentInChildren<Animator>();
+            Animator anim = bibleSpawns[i-1].GetComponentInChildren<Animator>();
             anim.SetTrigger("disappear");
         }
         yield return new WaitForSeconds(gameStats.bible.BibleCooldown);
